ad0b6913500f09fc0561b1a5d1e83d0d0137a471
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-12 18:16:45 +0300

    Question02

--- G-NET106-OOP-01/Program.cs
+++ G-NET106-OOP-01/Program.cs
@@ -20,6 +20,26 @@
             #endregion
 
 
+            #region Question02
+            /*
+             A) 1.All fields (Description, Weight, and DeliveryFee) are declared as public. 
+            External code can access and modify internal state directly without any validation or control .
+
+            2.Because Weight and DeliveryFee are public fields, external code can assign invalid or
+            logical boundary-breaking values (e.g., negative weight like Weight = -15.0 or negative fee like DeliveryFee = -50.0m).
+             
+            3.Changing the internal implementation details in the future
+            will break any external code directly accessing these fields.
+
+
+            B) Private fields and public properties improve this design by enforcing data validation 
+            (preventing invalid state such as negative Weight or DeliveryFee inside set accessors),
+            controlling access permissions (restricting modifications via read-only or init-only accessors),
+            encapsulating internal implementation (allowing internal storage changes without breaking external code), 
+            and enabling computed values (dynamically calculating values like fee based on weight).
+             
+             */
+            #endregion
         }
     }
 }
