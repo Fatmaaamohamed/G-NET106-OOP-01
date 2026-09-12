9b739c3df52058bf95e368270cc223439d6307a5
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-12 18:02:03 +0300

    Question01

--- G-NET106-OOP-01/Program.cs
+++ G-NET106-OOP-01/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            /* a) The DeliveryAddress variable will remain the same after modifing the variable that equal to
+             
+             DeliveryAddress variable beacuse DeliveryAddress is defined as  struct and struct is value type
+
+            (copied by value ,each copy is independent). */
+
+
+            /* B) Both variables will be modified because Customer variable is defined as class and class is
+       
+             refrence type.
+            */
+
+            #endregion
+
+
         }
     }
 }
